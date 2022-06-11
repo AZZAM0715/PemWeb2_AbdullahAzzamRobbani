0ef7f38f2b94613cefbe3353fdc6a10e1192ccac
azzam <abdu21083ti@student.nurulfikri.ac.id>
2022-06-11 16:46:45 +0700

    Praktikum 7 CI

---
+++ Praktikum_7/application/views/pages/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <img src="https://avatars.githubusercontent.com/u/59157541?s=400&u=fab48f1fe0c986d79b323c8efe327e6b06b14b1a&v=4" alt="Developer Photo's" class="shadow-lg">
+    <h2 class="text-center display-4"><i>@Azzzz.ar</i></h2>
+    <p class="w-50 text-center d-block m-auto">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita sit quo asperiores vel, pariatur consequatur omnis nesciunt repudiandae! Sit numquam tenetur aperiam magni, et illum vel nostrum adipisci aspernatur amet minus eius dolor harum quisquam provident asperiores assumenda iste natus laudantium mollitia pariatur. Obcaecati, excepturi in. In natus dolor ipsum!
+    </p>
+</div>
